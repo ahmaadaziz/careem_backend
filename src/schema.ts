d0d593673738a27a8f612e958c2e3d125e4bce66
Ahmad Aziz <ahamadaziz7@gmail.com>
2022-03-15 17:36:54 +0500

    created file structure and added User schema file

---
+++ src/schema.ts
@@ -0,0 +1,11 @@
+import { makeSchema } from "nexus";
+import { join } from "path";
+import * as types from "./graphql/index";
+
+export const schema = makeSchema({
+  types,
+  outputs: {
+    schema: join(process.cwd(), "schmea.graphql"),
+    typegen: join(process.cwd(), "nexus-typegen.ts"),
+  },
+});
